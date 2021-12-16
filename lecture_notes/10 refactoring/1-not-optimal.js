43033335758f6987fc618f8c91d3be1a789595b3
bugarenko <tbuharenko@kse.org.ua>
2021-12-16 22:48:57 +0200

    1-not-optimal.py

--- lecture_notes/10 refactoring/1-not-optimal.js
+++ lecture_notes/10 refactoring/1-not-optimal.js
@@ -1,4 +1,5 @@
-'use strict';
+'use strict'
+
 
 let shift = (offset, points) => {
   points.forEach((point) => {
@@ -19,7 +20,7 @@ let shift = (offset, points) => {
 const polyline = [
   { x: 0, y: 0 },
   { x: 10, y: 10 },
-  '{ "x": 20, "y": 20 }',
+ '{ "x": 20, "y": 20 }',
   { x: 30, y: 30 },
 ];
 
